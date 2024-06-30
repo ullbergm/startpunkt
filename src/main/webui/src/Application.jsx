3a82f41d93833571bbeca34477c70a1787ff5116
Magnus Ullberg <magnus@ullberg.us>
2024-06-30 18:30:00 -0400

    chore(enhancement): Update the layout and start adding bookmark support

--- src/main/webui/src/Application.jsx
+++ src/main/webui/src/Application.jsx
@@ -4,13 +4,17 @@ export function Application(props) {
   return (
     <div class="col d-flex align-items-start" style="transform: rotate(0);">
       <a href={props.app.url} target="_blank" class="stretched-link"></a>
-      {/* if the icon has :// in it, it's a URL to an image, otherwise it's a local icon, if it is an icon and has no : in it, then prepend mdi: to it */}
-      {props.app.icon.includes('://') ? (
+      {/* // if the icon has :// in it, it's a URL to an image, otherwise it's a local icon, if it is an icon and has no : in it, then prepend mdi: to it, if the icon is undefined then show nothing */}
+        {props.app.icon && props.app.icon.includes('://') ? (
         <img src={props.app.icon} alt={props.app.name} class="me-3" width="48" height="48" />
-      ) : (
-        <Icon icon={props.app.icon.includes(':') ? props.app.icon : `mdi:${props.app.icon}`} class="me-3 fs-2 text-primary" width="48" height="48" />
-      )}
-      
+        ) : props.app.icon && !props.app.icon.includes(':') ? (
+          <Icon icon={`mdi:${props.app.icon}`} class="me-3 fs-2 text-primary" width="48" height="48" />
+        ) : props.app.icon ? (
+          <Icon icon={props.app.icon} class="me-3 fs-2 text-primary" width="48" height="48" />
+        ) : (
+          ''
+        )}
+
       <div class="px-2">
         <h3 class="fw-bold mb-0 fs-4 text-body-emphasis text-uppercase">{props.app.name}</h3>
         <p class="accent text-uppercase">{props.app.info}</p>
