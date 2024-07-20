08529eac8feb3f225f50c80a54f6ff46b9dd989b
Magnus Ullberg <magnus@ullberg.us>
2024-07-20 09:43:23 -0400

    feat: configuration option for theme colors

--- src/main/java/us/ullberg/startpunkt/ConfigResource.java
+++ src/main/java/us/ullberg/startpunkt/ConfigResource.java
@@ -24,6 +24,36 @@ public class ConfigResource {
   @ConfigProperty(name = "quarkus.application.version", defaultValue = "0")
   public String version;
 
+  @ConfigProperty(name = "startpunkt.web.theme.light.bodyBgColor", defaultValue = "#F8F6F1")
+  public String lightThemeBodyBg;
+
+  @ConfigProperty(name = "startpunkt.web.theme.light.bodyColor", defaultValue = "#696969")
+  public String lightThemeBodyColor;
+
+  @ConfigProperty(name = "startpunkt.web.theme.light.emphasisColor", defaultValue = "#000000")
+  public String lightThemeEmphasisColor;
+
+  @ConfigProperty(name = "startpunkt.web.theme.light.textPrimaryColor", defaultValue = "#4C432E")
+  public String lightThemeTextPrimary;
+
+  @ConfigProperty(name = "startpunkt.web.theme.light.textAccentColor", defaultValue = "#AA9A73")
+  public String lightThemeTextAccent;
+
+  @ConfigProperty(name = "startpunkt.web.theme.dark.bodyBgColor", defaultValue = "#F8F6F1")
+  public String darkThemeBodyBg;
+
+  @ConfigProperty(name = "startpunkt.web.theme.dark.bodyColor", defaultValue = "#696969")
+  public String darkThemeBodyColor;
+
+  @ConfigProperty(name = "startpunkt.web.theme.dark.emphasisColor", defaultValue = "#000000")
+  public String darkThemeEmphasisColor;
+
+  @ConfigProperty(name = "startpunkt.web.theme.dark.textPrimaryColor", defaultValue = "#4C432E")
+  public String darkThemeTextPrimary;
+
+  @ConfigProperty(name = "startpunkt.web.theme.dark.textAccentColor", defaultValue = "#AA9A73")
+  public String darkThemeTextAccent;
+
   // GET endpoint to retrieve the list of bookmarks
   @GET
   public Response getConfig() {
@@ -32,8 +62,17 @@ public class ConfigResource {
 
   private Map<String, Object> generateConfig() {
     // Create a response with the configuration value
-    Map<String, Object> response =
-        Map.of("config", Map.of("web", Map.of("showGithubLink", showGithubLink, "title", title), "version", version));
+    Map<String, Object> response = Map.of("config",
+        Map.of("version", version, "web",
+            Map.of("showGithubLink", showGithubLink, "title", title, "theme",
+                Map.of("light",
+                    Map.of("bodyBgColor", lightThemeBodyBg, "bodyColor", lightThemeBodyColor,
+                        "emphasisColor", lightThemeEmphasisColor, "textPrimaryColor",
+                        lightThemeTextPrimary, "textAccentColor", lightThemeTextAccent),
+                    "dark",
+                    Map.of("bodyBgColor", darkThemeBodyBg, "bodyColor", darkThemeBodyColor,
+                        "emphasisColor", darkThemeEmphasisColor, "textPrimaryColor",
+                        darkThemeTextPrimary, "textAccentColor", darkThemeTextAccent)))));
 
     return response;
   }
