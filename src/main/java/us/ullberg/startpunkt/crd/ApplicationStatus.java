13f179f13f753a129a68e8e75ba4973df2bafd5f
Magnus Ullberg <magnus@ullberg.us>
2024-07-20 18:22:37 -0400

    style: more smell cleanup

--- src/main/java/us/ullberg/startpunkt/crd/ApplicationStatus.java
+++ src/main/java/us/ullberg/startpunkt/crd/ApplicationStatus.java
@@ -1,6 +1,10 @@
 package us.ullberg.startpunkt.crd;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+
 // Class representing the status of an Application custom resource
+// Include non-empty JSON properties in serialization
+@JsonInclude(JsonInclude.Include.NON_EMPTY)
 public class ApplicationStatus {
   // This class is currently empty, but it can be extended to include status fields
 }
--- src/main/java/us/ullberg/startpunkt/crd/BookmarkStatus.java
+++ src/main/java/us/ullberg/startpunkt/crd/BookmarkStatus.java
@@ -1,6 +1,10 @@
 package us.ullberg.startpunkt.crd;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+
 // Class representing the status of a Bookmark custom resource
+// Include non-empty JSON properties in serialization
+@JsonInclude(JsonInclude.Include.NON_EMPTY)
 public class BookmarkStatus {
   // This class is currently empty, but it can be extended to include status fields
   // For example, you might add fields like 'statusMessage', 'lastUpdated', etc.
